7f82b2e0a3d33929196a83d3a8bf024a63345d02
ivonild <ivodasilvapedro@gmail.com>
2024-02-25 15:39:28 +0000

    Initial code

---
+++ src/app/_components/mode-toggle.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+
+export function ModeToglle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="sm" className="h-6 w-6 px-0 ml-2">
+          <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        className="cursor-pointer  dark:bg-gray-900  bg-gray-200 py-2 px-4 rounded mt-1 "
+      >
+        <DropdownMenuItem onSelect={() => setTheme("light")} >
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onSelect={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onSelect={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
